9d8359e5c90db994856ea3a7f3dba32be31482cf
miro789 <mirohuyen5335.em@gmail.com>
2024-12-17 10:36:41 +0700

    update validation

--- src/main/java/vn/miro/dto/request/UserRequestDTO.java
+++ src/main/java/vn/miro/dto/request/UserRequestDTO.java
@@ -1,11 +1,13 @@
 package vn.miro.dto.request;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
+import org.springframework.format.annotation.DateTimeFormat;
+import vn.miro.util.PhoneNumber;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
 public class UserRequestDTO implements Serializable {
     @NotBlank(message = "First name must be not blank")
@@ -14,12 +16,21 @@ public class UserRequestDTO implements Serializable {
     @NotNull(message = "Last name must be not null")
     private String lastName;
 
-    @Pattern(regexp ="^\\d{10}$", message = "Email invalid format")
+    // @Pattern(regexp ="^\\d{10}$", message = "Email invalid format")
+    // custome anotation
+    @PhoneNumber
     private String phone;
 
     @Email(message = "Email invalid format")
     private String email;
 
+    @NotNull(message = "dateOfBirth must be not null")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @JsonFormat(pattern = "MM/DD/YYYY")
+    private Date dateOfBirth;
+
+    @NotEmpty
+    private List<String> permission;
 
     public UserRequestDTO(String firstName, String lastName, String phone, String email) {
         this.firstName = firstName;
@@ -42,5 +53,29 @@ public class UserRequestDTO implements Serializable {
 
     public String getEmail() {
         return email;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
     }
 }
